f4465f7c41c55e2f7eed6b4f4e39b054e3026c26
BrandyBecker <brandybeckerfsw@gmail.com>
2020-01-24 21:31:43 -0500

    Cleaned out Project.

--- mystery-machine/src/App.js
+++ mystery-machine/src/App.js
@@ -1,25 +1,37 @@
-//You will need to add to this file.
+/*
+  ---------------------------------------------------------------------------------------------------------------
+    You will need to add to this file.
+    Please remain within the hyphenated lines, as the surrounding elements all have CSS styles applied.
+  ---------------------------------------------------------------------------------------------------------------
+*/
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import Card from './Components/Card';
-import axios from 'axios';
 import Footer from './Components/Footer'
+// I have already imported the Card Component, it has styles applied to it.
+// You just need to Call the Component after you map through your Data and return the Data to the Card.
+import Card from './Components/Card';
+/* 
+  ---------------------------------------------------------------------------------------------------------------
+    Here you will import your Axios Dependancy.
+
+    
+  ---------------------------------------------------------------------------------------------------------------
+*/
 
 export default function App() {
 
-  const [char, setChar] = useState([])
+    /* 
+    ---------------------------------------------------------------------------------------------------------------
+      Here we will declare our State.
+      Here we will use Axios to fetch our Data.
+      Here we will set our returned Data to our State.
 
-  useEffect(() => {
-    axios
-      .get('https://cors-anywhere.herokuapp.com/https://scoobydooapi.herokuapp.com/api/characters')
-      .then(res => {
-        setChar(res.data)
-      })
-      .catch(err => {
-        console.log('Error:', err)
-      });
-  }, []);
+      Please look in the README.md to figure out the URL you'll use to fetch Data.
+      
 
+
+      ---------------------------------------------------------------------------------------------------------------
+    */
   return (<>
     <img
         className="scooby-logo"
@@ -27,11 +39,17 @@ export default function App() {
         src="https://upload.wikimedia.org/wikipedia/commons/2/28/Be_Cool%2C_Scooby-Doo%21_logotype.png"
       />
     <div className="flex-div">
-      {
-        char.map(character => (
-           <Card character={character} key={character.id}/>
-     ))
-      }
+      {/*
+        ---------------------------------------------------------------------------------------------------------------
+        Here, we can map over our data inside of this Div with the className="flex-div".
+        This div has styles applied to it. 
+        - You will Map over your Data
+        - You will return the <Card/> component with a key
+
+
+
+        ---------------------------------------------------------------------------------------------------------------
+      */}
     </div>
     <Footer/>
     </>
--- mystery-machine/src/Components/Card.js
+++ mystery-machine/src/Components/Card.js
@@ -5,10 +5,16 @@ import React from 'react';
 const Card = (props) => {
   return (
     <div className="card-Parent">
-      <h1 className="card-title">{props.character.name} </h1>
-      <img className="card-image" alt="" src={props.character.imageurl} />
-      <p className="card-gender">Gender: {props.character.gender} </p>
-      <p className="card-id">id: {props.character.id} </p>
+      <h1 className="card-title">
+          Name:
+      </h1>
+      <img className="card-image" alt="" src="" />
+      <p className="card-gender">
+          Gender:
+      </p>
+      <p className="card-id">
+          ID: 
+      </p>
     </div>
   );
 };
